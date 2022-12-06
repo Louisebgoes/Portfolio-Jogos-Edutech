f26890bdd6a1f048607eb4542bf3b6290e873356
Louise <louiserpb@hotmail.com>
2022-12-06 10:47:23 -0300

    adicionando os jogos

--- main.js
+++ main.js
@@ -31,65 +31,18 @@ function creatCard(date, day, games) {
 
 document.querySelector("#app").innerHTML = `
   <main id="cards">
-    ${creatCard(
-      "Bruna",
-      "6º ano",
-      createGame(
-        "Calculadora de Suspiro",
-        "https://scratch.mit.edu/projects/700642787"
-        )
-    )}
-
-    ${creatCard(
-      "Gabriel HENRIQUE",
-      "7º ano",
-      createGame(
-        "Corrida científica",
-        "https://scratch.mit.edu/projects/723148726"
-      )
-    )}
-
-    ${creatCard(
-      "Gustavo Bueno",
-      "9º ano",
-      createGame(
-        "Monster Trash Battle",
-        "https://scratch.mit.edu/projects/709514538"
-      )
-    )}
-
-    ${creatCard(
-      "Gustavo Maciel",
-      "8º ano",
-      createGame(
-        "As aventuras de Odin",
-        "https://scratch.mit.edu/projects/715456382"
-      )
-    )}
-
-    ${creatCard(
-      "Hylber",
-      "6º ano",
-      createGame(
-        "As aventuras de João o Pão e Juninho o taco",
-        "https://scratch.mit.edu/projects/709518970"
-      )
-    )}
-
-    ${creatCard(
-      "Iasmin",
-      "6º ano",
-      createGame(
-        "Desafio nas alturas",
-        "https://scratch.mit.edu/projects/730885451"
-      )
-    )}
-
-    ${creatCard("Isabelli", "7º ano", createGame("Jogo do Scratch"))}
-    ${creatCard("Kauanny", "6º ano", createGame("Jogo do Scratch"))}
-    ${creatCard("Maria Eduarda", "7º ano", createGame("Jogo do Scratch"))}
-    ${creatCard("Pedro", "7º ano", createGame("Jogo do scratch"))}
-    ${creatCard("Roberto", "6º ano", createGame("Flap bird da Shopee"))}
-    ${creatCard("Victor ", "7º ano", createGame("Jogo do Scratch"))}
+    ${creatCard("Bruna","6º ano", createGame("Calculadora de Suspiro","https://scratch.mit.edu/projects/700642787"))}
+    ${creatCard("Davi", "6º ano", createGame("Calculadora de Suspiro", "https://scratch.mit.edu/projects/691200372"))}
+    ${creatCard("Diogo", "7º ano", createGame("Calculadora de Suspiro", "https://scratch.mit.edu/projects/698544222"))}
+    ${creatCard("Gabriel HENRIQUE","7º ano",createGame("Corrida científica","https://scratch.mit.edu/projects/723148726"))}
+    ${creatCard("Gustavo Bueno","9º ano",createGame("Monster Trash Battle","https://scratch.mit.edu/projects/709514538"))}
+    ${creatCard("Gustavo Maciel","8º ano",createGame("As aventuras de Odin","https://scratch.mit.edu/projects/715456382"))}
+    ${creatCard("Hylber","6º ano", createGame("As aventuras de João o Pão e Juninho o taco","https://scratch.mit.edu/projects/709518970"))}
+    ${creatCard("Iasmin","6º ano",createGame("Desafio nas alturas","https://scratch.mit.edu/projects/730885451"))}
+    ${creatCard("Kauanny", "6º ano", createGame("MIA","https://scratch.mit.edu/projects/721973410"))}
+    ${creatCard("Pedro T.", "7º ano", createGame("Calculadora Legal","https://scratch.mit.edu/projects/709508166"))}
+    ${creatCard("Pedro L.", "7º ano", createGame("Movimento Browniano", "https://scratch.mit.edu/projects/709509100"))}
+    ${creatCard("Roberto", "6º ano", createGame("Salve o Calvo","https://scratch.mit.edu/projects/727495546"))}
+    ${creatCard("Victor ", "7º ano", createGame("Jogo do avião","https://scratch.mit.edu/projects/730879433"))}
   </main>
 `;
